61c88a78407ccbb57fc9b0737c18a4469ca18527
Trev <trevg@aol.com>
2019-07-02 11:26:01 -0700

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Trevor Griffiths on 7/1/19.
+//  Copyright © 2019 Trevor Griffiths. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
